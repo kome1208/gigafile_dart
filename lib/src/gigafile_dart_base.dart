e8975b526633765f5eab23db8d59fca3495c93e4
kome1208 <kome1208@outlook.jp>
2025-08-17 21:23:05 +0900

    export model

--- lib/src/gigafile_dart_base.dart
+++ lib/src/gigafile_dart_base.dart
@@ -1,3 +1,8 @@
 export 'gigafile.dart';
 export 'uploader.dart';
 export 'downloader.dart';
+export 'model/download_request.dart';
+export 'model/download_result.dart';
+export 'model/matomete_get_url.dart';
+export 'model/upload_request.dart';
+export 'model/upload_result.dart';
